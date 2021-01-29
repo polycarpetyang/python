db6449a5090c50ad8918e08daaf67396bdbd51d2
Polycarp Etyang Mubweka <51959993+polycarpetyang@users.noreply.github.com>
2021-01-29 09:55:39 +0300

    Update bus_fare_challenge.py

--- challenges/week_1/bus_fare_challenge.py
+++ challenges/week_1/bus_fare_challenge.py
@@ -1,1 +1,18 @@
 # WRITE YOUR CODE SOLUTION HERE
+from datetime import date
+date = date.today()
+
+print(date )
+
+date_ =str(input('Enter the date(for example:09 02 2019):'))
+day_name= ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun']
+day = datetime.datetime.strptime(date, '%d %m %Y').weekday()
+print(day_name[day]) 
+
+if day_name [0,1,2,3,4,5]:
+    fare = '100'
+    elif day_name [6] :
+        fare = '60'
+        else:
+            fare = '80'
+print(fare)
